2c9c779d13f512d7f5174466201b2687f4438b42
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-10-25 15:48:13 -0400

    updated app.listen

--- server.js
+++ server.js
@@ -99,6 +99,6 @@ app.get("/deals", function(req, res) {
       })
   })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("App running on port 3000!");
   });
